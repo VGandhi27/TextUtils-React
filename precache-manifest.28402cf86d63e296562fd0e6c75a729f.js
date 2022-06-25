65cf889b3b6dabf1d967a25a668205012b6267cc
Vidushi Gandhi <curiousvidushi27@gmail.com>
2022-06-25 13:34:20 +0530

    Updates

---
+++ precache-manifest.28402cf86d63e296562fd0e6c75a729f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fceba85d2a5df80626ec",
+    "url": "/TextUtils-React/static/css/main.980e82f1.chunk.css"
+  },
+  {
+    "revision": "fceba85d2a5df80626ec",
+    "url": "/TextUtils-React/static/js/main.fceba85d.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "47a61aeb0ce2eb63fb5a",
+    "url": "/TextUtils-React/static/js/2.47a61aeb.chunk.js"
+  },
+  {
+    "revision": "0d79c3c2a5762a692cdb",
+    "url": "/TextUtils-React/static/js/runtime~main.0d79c3c2.js"
+  },
+  {
+    "revision": "5b19e4488f9de26a3a08aa037398f259",
+    "url": "/TextUtils-React/index.html"
+  }
+];
